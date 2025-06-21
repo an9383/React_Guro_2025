94a9e15026055675f4e711bc03877468a73576aa
an9383 <an9383@gmail.com>
2025-06-21 12:42:24 +0900

    all

--- test1_0621/src/components/Table.jsx
+++ test1_0621/src/components/Table.jsx
@@ -1,30 +1,30 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import Tr from './Tr';
+
+
 
 const data = [
-    {name:"john", kor:90, math: 89, eng:95},
-    {name:"john", kor:90, math: 89, eng:95},
-    {name:"john", kor:90, math: 89, eng:95},
+    {name: "john", kor: 90, math: 89, eng: 95, sci: 100},
+    {name: "john", kor: 90, math: 89, eng: 95, sci: 100},
+    {name: "john", kor: 90, math: 89, eng: 95, sci: 100},
 ]
 
-const Td= ({scores}) => {
-    <tr>
-        <Td>나는 Td야{scores}</Td>
-    </tr>
-}
-
-const Tr= ({scores}) => {
-    <tr>
-        <Td scores={scores}/>
-    </tr>
-}
-
-
 const Table = () => {
-    const [scores, setScores] = useState(data)
+    const [scores, setScores] = useState(data);
   return (
-    <Table>
-        {scores.map(item=>(<Tr item={item}/>))}
-    </Table>
+    <>
+        <table border='1'>
+            <thead>
+                <tr>
+                    {Object.keys(scores[0]).map(item=>(<th>{item}</th>))}
+                </tr>
+            </thead>
+            <tbody>
+                {scores.map(item => (<Tr item={item}/>))}
+            </tbody>
+                
+        </table>
+    </>
   )
 }
 
