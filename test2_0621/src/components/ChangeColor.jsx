615e36a5ae59548ea0aa0e0a3ffba8b98522ff7a
an9383 <an9383@gmail.com>
2025-06-21 17:16:58 +0900

    all

--- test2_0621/src/components/ChangeColor.jsx
+++ test2_0621/src/components/ChangeColor.jsx
@@ -6,19 +6,39 @@ const style = {
     backgroundColor: 'black',
     border: "1px solid black",
 }
+
+const initialState = {color:'black'}
+
+const colorReducer = (state, action) => {
+  switch (action.type) {
+    case 'red':
+      return {...state,color: 'red' };
+    case 'blue':
+      return {...state, Color: 'blue' };
+    default:
+      throw state;
+  }
+}
 const ChangeColor = () =>{
     const [color, setColor] = React.useState('black');
     const [textColor, setTextColor] = React.useState('white');
+    const [state, dispatch] = React.useReducer(colorReducer, initialState);
+
+    // const handleClick = (param) => {
+    //     setColor(param);
+    // }
 
     const handleClick = (param) => {
-        setColor(param);
+        dispatch({type: param})
     }
   return (
     <>
-      <div style={{...style, backgroundcolor:color, color: color}}>안녕하세요.!!!</div>
+      {/* <div style={{...style, backgroundcolor:color, color: color}}>안녕하세요.!!!</div>
       <button onClick={() => handleClick("red")}>red</button>
       <button onClick={() => handleClick("green")}>Green</button>
-      <button onClick={() => setTextColor("blue")}>text color</button>
+      <button onClick={() => setTextColor("blue")}>text color</button> */}
+      <div style={{...style, backgroundColor: state.color}}>안녕하세요.!!!</div>
+      <button onClick={() => dispatch({type: 'red'})}>red</button>
       
     </>
   )
