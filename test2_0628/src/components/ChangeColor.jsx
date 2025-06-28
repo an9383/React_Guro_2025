a42e800277781ea83438a90468a88f278c2f5e30
an9383 <an9383@gmail.com>
2025-06-28 11:38:58 +0900

    all

---
+++ test2_0628/src/components/ChangeColor.jsx
@@ -0,0 +1,37 @@
+import React, { useState} from 'react'
+import { useSelector } from 'react-redux'
+
+
+const style ={
+    width: "100px",
+    height: "100px",
+    backgroundColor: 'black',
+    border: "1px solid black",
+    color: "white",
+}
+
+
+
+
+const ChangeColor = () => {
+    const state = useSelector(state=>state.color)
+    // const [state, dispatch] = useReducer(colorReducer, initialState);
+    // const handleClick = (param) => {
+    //     param === "red" ? dispatch({type:"RED"}) : dispatch({type:"BLUE"})
+    // }
+    const handleClick = (param)=>{
+        console.log("state", state)
+        return param
+    }
+
+  return (
+    <>
+        <div style={{...style}}>안녕하세요!!!</div>
+        <button onClick={()=>handleClick('red')}>red</button>
+        <button onClick={()=>handleClick('blue')}>blue</button>
+        
+    </>
+  )
+}
+
+export default ChangeColor;
