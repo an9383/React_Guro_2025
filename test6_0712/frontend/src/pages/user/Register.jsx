f1ac2d3a8026df227c84adc8d679e3e3b1145a45
an9383 <an9383@gmail.com>
2025-07-12 16:03:58 +0900

    all

--- test6_0712/frontend/src/pages/user/Register.jsx
+++ test6_0712/frontend/src/pages/user/Register.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import React, { useState, useCallback, useEffect } from 'react';
 import { Input, Button, Card, Space, Form, message } from 'antd'; // Import Form and message
 import { useDispatch, useSelector } from 'react-redux'; // Import useSelector
