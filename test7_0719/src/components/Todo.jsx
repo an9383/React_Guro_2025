e9e1ca4a3916de6de1e1b82d9ad12000b10c5ee3
an9383 <an9383@gmail.com>
2025-07-19 15:03:38 +0900

    all

---
+++ test7_0719/src/components/Todo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { MdCheckBox, MdCheckBoxOutLineblank, MdRemoveCircleOutLine} from 'react-icons'
+
+const Todo = () => {
+  return (
+    <div>
+        <div>
+            <MdCheckBox style={{fotnSize:"1.5rem", color:"#339af0"}} />
+        </div>
+        <div>
+            javaScript study
+        </div>
+    </div>
+  )
+}
+
+export default Todo
