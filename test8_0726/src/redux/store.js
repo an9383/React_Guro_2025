c7e11ba49df86c34f82253d54421063619cdc05b
an9383 <an9383@gmail.com>
2025-08-03 06:09:49 +0900

    all

--- test8_0726/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todoSlice from './todoSlice';
-
-const store = configureStore({
-    reducer: {
-        todoSlice, 
-    }
-})
-export default store;
