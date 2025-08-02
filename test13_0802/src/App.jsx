edbb8f43b2e088a7619eadad049bb675849b7492
an9383 <an9383@gmail.com>
2025-08-02 16:22:42 +0900

    all

--- test13_0802/src/App.jsx
+++ test13_0802/src/App.jsx
@@ -3,8 +3,20 @@ import { useState , useMemo} from 'react'
 import './App.css'
 import ProductDisplay from './components/ProductDisplay'
 
+const initialStateUser = {
+  name:'김철수',
+  age:30,
+  count:0,  
+}
 function App() {
 const [text,setText] = useState('')
+
+const [user, setUser] = useState(initialStateUser);
+ 
+const memorizedUser = useMemo(() => (
+   {name: user.name, age:user.age},[user]));
+
+
 const items = useMemo(() => [
   {id:1, name:'사고'},
   {id:2, name:'바나나'},
@@ -13,8 +25,15 @@ const items = useMemo(() => [
 
   return (
     <>
+    <div>
       <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
       <ProductDisplay products={items} filterText={text} />
+    </div>
+    <div>
+      <input type="text" value={user.name} onChange={e=>setUser(prev=>({...prev, name:e.target.value}))} />
+      <input type="number" value={user.age} onChange={e=>setUser(prev=>({...prev, age:e.target.value}))} />
+    </div>
+      
     </>
   )
 }
---
+++ test13_0802/src/components/UserInfo.jsx
@@ -0,0 +1,17 @@
+import React,{memo} from 'react'
+
+
+const UserInfo = memo(({user})) =>{
+    return (
+        <div>
+{user.name} {user.age}
+        </div>
+    )
+}
+const UserInfo = () => {
+  return (
+    <div>UserInfo</div>
+  )
+}
+
+export default UserInfo
